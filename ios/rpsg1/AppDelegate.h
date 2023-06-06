58b408b508db7260d2af82f0b63c382e3eee1fd6
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2023-06-06 23:30:58 +0530

    Revert "Sivakumar: Sample rn ios ext"
    This reverts commit 22266654f0ed08ad9032608cd036879e87bbec61.

--- ios/rpsg1/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-#import <REIOSSDK/REIOSSDK.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate,REiosNotificationReceiver,REiosSmartLinkReceiver>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end
